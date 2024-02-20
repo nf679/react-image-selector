f7ff7e6b88d6502ba591e9ed99f3df0df8400678
nf679 <nicola.farmer@stfc.ac.uk>
2024-02-20 15:22:28 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ImageSelectorComponent from './ImageSelector';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Nicola Image Selection Test App</h1>
+      <ImageSelectorComponent />
     </div>
   );
 }
